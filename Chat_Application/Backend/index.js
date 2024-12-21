0f88b928de3d11ca514f8a333c197da44224dbff
Ayush Pawar <ayushap1105@gmail.com>
2024-12-21 12:08:40 +0530

    Day 11 of Creating Chat Application connected with mongodDB

--- Chat_Application/Backend/index.js
+++ Chat_Application/Backend/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 dotenv.config();
+
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Server is connected to DataBase");
+  } catch (err) {
+    console.log("Server is NOT connected to DataBase", err.message);
+  }
+};
+connectDb();
+
 app.get("/", (req, res) => {
   res.send("API is working1");
 });
-const PORT = process.env.PORT || 1111;
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log("Server is Running..."));

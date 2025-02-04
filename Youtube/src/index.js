339527ec90a44fd2905454a549baa2298d07f4d7
Ayush Pawar <ayushap1105@gmail.com>
2025-02-04 17:35:39 +0530

    Learning Backend

--- Youtube/src/index.js
+++ Youtube/src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !");
+  });
 
 /*
 import express from "express";
